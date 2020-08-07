fa6d82f6391252dd5a2da0607d5c00f5f4704862
Marcos Robles <ecko_lob@outlook.com>
2020-08-07 12:50:33 -0500

    ADD partialView for restaurants , View Component  to count Restaurants and CRUD con scafolding

---
+++ OdeToFood2/Pages/R2/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood2.Core;
+using OdeToFood2.Data;
+
+namespace OdeToFood2.Pages.R2
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly OdeToFood2.Data.OdeToFoodDbContext _context;
+
+        public DetailsModel(OdeToFood2.Data.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Restaurant = await _context.Restaurants.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Restaurant == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ OdeToFood2/Pages/R2/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood2.Core;
+using OdeToFood2.Data;
+
+namespace OdeToFood2.Pages.R2
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OdeToFood2.Data.OdeToFoodDbContext _context;
+
+        public IndexModel(OdeToFood2.Data.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
